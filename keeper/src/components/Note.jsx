b15048baca15fd23dc6dac840684f8d058755367
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-16 17:53:07 +0100

    Style the Keeper app

--- keeper/src/components/Note.jsx
+++ keeper/src/components/Note.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Note() {
   return (
-    <div>
+    <div className="note">
       <h1>This is the note title</h1>
       <p>This is the note content</p>
     </div>
