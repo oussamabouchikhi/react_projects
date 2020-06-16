460094d5a78998a8ea9a35facced5b5926c0d596
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-16 17:47:21 +0100

    Create components

---
+++ keeper/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <h1>This is the note title</h1>
+      <p>This is the note content</p>
+    </div>
+  );
+}
+
+export default Note;
--- keeper/src/index.js
+++ keeper/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from "./components/App";
 ReactDOM.render(
-  <h1>Hello world</h1>,
+  <App />,
   document.getElementById('root')
 );
