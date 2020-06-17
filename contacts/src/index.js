d5060d50690319ce80a2a76c17acc04983c4e0aa
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-17 13:47:13 +0100

    Extract Card component

--- contacts/src/index.js
+++ contacts/src/index.js
@@ -1,35 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function Card() {
+  return (
+    <div>
+      <h2>Beyonce</h2>
+      <img
+        src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        alt="avatar"
+      />
+      <p>+123 456 789</p>
+      <p>b@beyonce.com</p>
+    </div>
+  );
+}
+
 ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
-
-    <h2>Beyonce</h2>
-    <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      alt="avatar"
-    />
-    <p>+123 456 789</p>
-    <p>j@tatham.com</p>
-
-    <h2>Beyonce</h2>
-    <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      alt="avatar"
-    />
-    <p>+123 456 789</p>
-    <p>j@tatham.com</p>
-
-    <h2>Beyonce</h2>
-    <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      alt="avatar"
-    />
-    <p>+123 456 789</p>
-    <p>j@tatham.com</p>
-
-
+    <Card />
+    <Card />
+    <Card />
   </div>,
   document.getElementById('root')
 );
