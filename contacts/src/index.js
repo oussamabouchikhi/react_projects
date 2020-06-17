55cb345f7ffce8a5800e286d73ea6c8ae510dff1
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-17 14:10:05 +0100

    Passing props & make Card component reusable

--- contacts/src/index.js
+++ contacts/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Card() {
+function Card(props) {
   return (
     <div>
-      <h2>Beyonce</h2>
+      <h2>{props.name}</h2>
       <img
-        src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        src={props.img}
         alt="avatar"
       />
-      <p>+123 456 789</p>
-      <p>b@beyonce.com</p>
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
     </div>
   );
 }
@@ -18,9 +18,12 @@ function Card() {
 ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
-    <Card />
-    <Card />
-    <Card />
+    <Card
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      tel="+123 456 789"
+      email="b@beyonce.com"
+    />
   </div>,
   document.getElementById('root')
 );
