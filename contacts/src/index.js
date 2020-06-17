afd0fd459f88d3e89ece6416c037f5009e394649
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-17 13:45:01 +0100

    Create basic structure

--- contacts/src/index.js
+++ contacts/src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1>My Contacts</h1>
+
+    <h2>Beyonce</h2>
+    <img
+      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      alt="avatar"
+    />
+    <p>+123 456 789</p>
+    <p>j@tatham.com</p>
+
+    <h2>Beyonce</h2>
+    <img
+      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      alt="avatar"
+    />
+    <p>+123 456 789</p>
+    <p>j@tatham.com</p>
+
+    <h2>Beyonce</h2>
+    <img
+      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      alt="avatar"
+    />
+    <p>+123 456 789</p>
+    <p>j@tatham.com</p>
+
+
+  </div>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
