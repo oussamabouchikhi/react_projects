35732d643de0cef365a5e12d9e10a0e9b673da3f
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-21 14:55:18 +0100

    Reusing Card component

--- contacts/src/index.js
+++ contacts/src/index.js
@@ -24,6 +24,18 @@ ReactDOM.render(
       tel="+123 456 789"
       email="b@beyonce.com"
     />
+    <Card
+    name="Jack Bauer"
+    img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+    tel="+987 654 321"
+    email="jack@nowhere.com"
+  />
+  <Card
+    name="Chuck Norris"
+    img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+    tel="+918 372 574"
+    email="gmail@chucknorris.com"
+  />
   </div>,
   document.getElementById('root')
 );
