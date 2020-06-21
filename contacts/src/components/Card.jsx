614ae8ade65e0fce249632af632d6ae9dc8d254c
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-21 15:41:27 +0100

    Create Avatar component

--- contacts/src/components/Card.jsx
+++ contacts/src/components/Card.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Avatar from "./Avatar";
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img className="circle-img" src={props.img} alt="avatar_img" />
+        <Avatar img={props.img} />
       </div>
       <div className="bottom">
         <p className="info">{props.tel}</p>
