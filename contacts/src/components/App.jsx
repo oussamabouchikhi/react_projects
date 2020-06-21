baff4c3a2570c3e15f24005cf78a8130a1849ff8
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-21 16:05:16 +0100

    Mapping data through Card components

--- contacts/src/components/App.jsx
+++ contacts/src/components/App.jsx
@@ -2,29 +2,23 @@ import React from 'react';
 import Card from './Card';
 import contacts from '../contact';
 
+function createCard(contact) {
+    return (
+        <Card
+            key={contact.id} 
+            name={contact.name}
+            img={contact.imgURL}
+            tel={contact.phone}
+            email={contact.email}
+        />
+    );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
-     
+      {contacts.map(createCard)}
     </div>
   );
 }
