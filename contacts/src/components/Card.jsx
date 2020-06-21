2d949548bf0e919428002a3ccdb63d9c4bdb3443
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-21 15:44:43 +0100

    Create Detail component

--- contacts/src/components/Card.jsx
+++ contacts/src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Avatar from "./Avatar";
+import Detail from "./Detail";
 
 function Card(props) {
   return (
@@ -9,8 +10,8 @@ function Card(props) {
         <Avatar img={props.img} />
       </div>
       <div className="bottom">
-        <p className="info">{props.tel}</p>
-        <p className="info">{props.email}</p>
+        <Detail info={props.tel} />
+        <Detail info={props.email} />
       </div>
     </div>
   );
---
+++ contacts/src/components/Detail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Detail(props) {
+  return <p className="info">{props.info}</p>;
+}
+
+export default Detail;
