d5cbf0da03a4947519fc28998ef1d955009a72ec
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-21 15:14:37 +0100

    Style the app

--- contacts/src/index.js
+++ contacts/src/index.js
@@ -3,21 +3,22 @@ import ReactDOM from 'react-dom';
 
 function Card(props) {
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <img
-        src={props.img}
-        alt="avatar"
-      />
-      <p>{props.tel}</p>
-      <p>{props.email}</p>
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <img className="circle-img" src={props.img} alt="avatar_img" />
+      </div>
+      <div className="bottom">
+        <p className="info">{props.tel}</p>
+        <p className="info">{props.email}</p>
+      </div>
     </div>
   );
 }
 
 ReactDOM.render(
   <div>
-    <h1>My Contacts</h1>
+    <h1 className="heading">My Contacts</h1>
     <Card
       name="Beyonce"
       img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
