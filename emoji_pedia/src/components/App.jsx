fa4f0c98878ff6fc30dd37758226b04603fe26b3
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-22 14:35:33 +0100

    Create Entry component

--- emoji_pedia/src/components/App.jsx
+++ emoji_pedia/src/components/App.jsx
@@ -1,20 +1,5 @@
 import React from "react";
-
-function Term(props) {
-  return (
-    <div className="term">
-      <dt>
-        <span className="emoji" role="img" aria-label={props.name}>
-          {props.emoji}
-        </span>
-        <span>{props.name}</span>
-      </dt>
-      <dd>
-        {props.description}
-      </dd>
-    </div>
-  );
-}
+import Entry from "./Entry";
 
 function App() {
   return (
@@ -24,17 +9,17 @@ function App() {
       </h1>
 
       <dl className="dictionary">
-        <Term
+        <Entry
           emoji="💪"
           name="Tense Biceps"
           description='“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.'
         />
-        <Term
+        <Entry
           emoji="🙏"
           name="Person With Folded Hands"
           description='Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.'
         />
-        <Term
+        <Entry
           emoji="🤣"
           name="Rolling On The Floor, Laughing"
           description='  This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.'
