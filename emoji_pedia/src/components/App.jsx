e99a5aa6fe2992d2f9063359508b9beceb69c4aa
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-22 14:54:51 +0100

    Map through emojipedia data entries

--- emoji_pedia/src/components/App.jsx
+++ emoji_pedia/src/components/App.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+
+function createEntry(entry) {
+  return (
+    <Entry 
+      emoji={entry.emoji}
+      name={entry.name}
+      description={entry.meaning}
+    />
+  );
+}
 
 function App() {
   return (
@@ -7,24 +19,8 @@ function App() {
       <h1>
         <span>emojipedia</span>
       </h1>
-
       <dl className="dictionary">
-        <Entry
-          emoji="💪"
-          name="Tense Biceps"
-          description='“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.'
-        />
-        <Entry
-          emoji="🙏"
-          name="Person With Folded Hands"
-          description='Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.'
-        />
-        <Entry
-          emoji="🤣"
-          name="Rolling On The Floor, Laughing"
-          description='  This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.'
-        />
-
+        {emojipedia.map(createEntry)}
       </dl>
     </div>
   );
