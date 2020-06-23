cc824d850d99dd9962544fb0b2c10e73a787375b
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-23 15:15:30 +0100

    Map through notes dynamically

--- keeper/src/components/App.jsx
+++ keeper/src/components/App.jsx
@@ -1,13 +1,20 @@
-import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
+import React  from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Note   from './Note';
+import notes  from '../notes';
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(note => (
+        <Note 
+          key={note.key}
+          title={note.title}
+          content={note.content}
+        />
+      ))}
       <Footer />
     </div>
   );
