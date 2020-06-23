125963831747edd4b3e7813a8063c82c19416927
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-23 19:18:10 +0100

    Render Login conditionally

--- login/src/components/App.jsx
+++ login/src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Login from './Login';
 
+let isLoggedin = true;
+
 function App() {
     return (
         <div className="container">
-        <h1>Hello</h1>
-        <Login />
+            {isLoggedin ? <h1>Hello again</h1> : <Login />}
         </div>
     );
 }
