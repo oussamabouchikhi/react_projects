bb2ada228e5d6a05a06e9bac89765fd598a392c6
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-23 19:02:19 +0100

    Create Input component

--- login/src/components/Login.jsx
+++ login/src/components/Login.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Input from './Input';
 
 function Login() {
     return (
         <form className="form">
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
+            <Input type='text' placeholder='Username' />
+            <Input type='password' placeholder='Password' />
             <button type="submit">Login</button>
         </form>
     );
