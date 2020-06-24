7349a622fd4b8620d623eb494deb09865863a9c2
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-06-24 15:05:29 +0100

    Re-render DOM

--- counter/src/index.js
+++ counter/src/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+let count = 0;
+
+function increase() {
+  count++;
+  console.log(`Count= ${count}`);
+  // Re-render DOM to increase counter
+  ReactDOM.render(
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+    </div>,
+    document.getElementById("root")
+  );
+}
+
 ReactDOM.render(
   <div className="container">
-    <h1>0</h1>
-    <button>+</button>
+    <h1>{count}</h1>
+    <button onClick={increase}>+</button>
   </div>,
   document.getElementById("root")
 );
